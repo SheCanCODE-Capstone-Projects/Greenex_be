41be32432ad57f0612c903f135d85e09df3c85a6
SarahNiyonziza <sarahniyonziza@gmail.com>
2025-12-11 16:20:23 +0200

    Implementation of Contact Us feature

--- src/main/java/org/example/greenexproject/config/WebConfig.java
+++ src/main/java/org/example/greenexproject/config/WebConfig.java
@@ -2,14 +2,30 @@ package org.example.greenexproject.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8888",  // Swagger UI + backend same port
+                        "http://localhost:3000",  // React or frontend
+                        "http://127.0.0.1:5500"   // Static HTML testing
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(false);
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new org.example.greenexproject.Config.CustomPageableArgumentResolver());
+        resolvers.add(new CustomPageableArgumentResolver());
     }
 }
